cf13a353ed3f424e5dda4eb1c5f7a292cb0d92e9
GuillaumeDMMarion <guillaume.d.m.marion@gmail.com>
2019-01-06 17:29:54 +0100

    Changed test script
    Adapted the test script to align with final training model.

--- sumoDqnTest.py
+++ sumoDqnTest.py
@@ -51,13 +51,13 @@ shape_phase = env.observation_space.spaces['phase'].shape
 shape_vehicle = env.observation_space.spaces['vehicle'].shape
 
 ### Input/Output suffix
-model = load_model("checkpoint_reward_-1445235.0.h5f")
+model = load_model("reward_-1019065.0.h5f")
 
 ### Policy, Memory & Agent set-up.
-policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05, nb_steps=40000)
-memory = SequentialMemory(limit=20000, window_length=1)
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, policy=policy, batch_size=32, gamma=.99, nb_steps_warmup=1000, target_model_update=.001, enable_double_dqn=False, enable_dueling_network=False) # target_model_update=10000 # dueling_type='avg'
-dqn.processor = MultiInputProcessor(2)
+policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.01, value_test=.01, nb_steps=100000)
+memory = SequentialMemory(limit=50000, window_length=1)
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, policy=policy, batch_size=64, gamma=.95, nb_steps_warmup=2000, target_model_update=.001)
+dqn.processor = MultiInputProcessor(2)	
 dqn.compile(optimizer=Adam(lr=.001))
 
 # Test the loaded model & weights.
